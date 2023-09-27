7f657e46361a93cf787e05676d5bd4249f2b819c
Pavlo Polosenko <pavlopolosenko@gmail.com>
2023-09-27 17:33:00 +0300

    add move for bubles and death animation

--- Bubles/buble.gd
+++ Bubles/buble.gd
@@ -1,19 +1,39 @@
 extends Area2D
 
 
+@onready var buble_sprite: Sprite2D = get_node("Sprite2D")
+@onready var death_sprite: Sprite2D = get_node("Death")
+@onready var death_timer: Timer = get_node("Timer")
+
+@export var move_speed = 3
+var direction
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var scaling = randf_range(2, 5)
+	scale.x *= scaling
+	scale.y *= scaling
+	var random_angle = randf() * 2 * PI
+	direction = Vector2(cos(random_angle), sin(random_angle))
+	direction = direction.normalized()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
-	
+	position += move_speed*direction
+
+
 func _input(event):
 	if event is InputEventScreenTouch:
 		if event.pressed and event.index == 0:
-			pass
+			buble_sprite.visible = false
+			death_sprite.visible = true
+			set_process(false)
+			death_timer.start()
 			# Touch event started on the Area2D
 			# Add your code here
+
+
+func _on_timer_timeout():
+	queue_free()
